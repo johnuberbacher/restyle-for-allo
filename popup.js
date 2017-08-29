b8a6aafd002a82579f8aed7318e9d275a35fc61c
John Uberbacher <johnuberbacher@gmail.com>
2017-08-28 20:37:13 -0400

    Cleaning up and organizing

--- popup.js
+++ popup.js
@@ -4,7 +4,6 @@ function save_options() {
     disableSticker: disableSticker
   }, 
   function() {
-    // Update status to let user know options were saved.
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(function() {
