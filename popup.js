7f03c87d8dd634e0a0a0c8c4e95381c850aa485b
John Uberbacher <johnuberbacher@gmail.com>
2018-02-23 23:12:42 -0700

    Small UI updates + Popup window functionality
    Added in an option to open Allo in a new pop up window when clicking on
    the extension. Will probably also make a button or hotkey command soon.

--- popup.js
+++ popup.js
@@ -1,5 +1,25 @@
+function load_window() {
+ chrome.windows.getCurrent(function(win) {
+	var width = 860;
+	var height = 630;
+	var left = ((screen.width / 2) - (width / 2)) + win.left;
+	var top = ((screen.height / 2) - (height / 2)) + win.top;
+
+	chrome.windows.create({
+		url: 'https://allo.google.com/web',
+		width: width,
+		height: height,
+		top: Math.round(top),
+		left: Math.round(left),
+		type: 'popup'
+	});
+ });
+}
+
 function save_options() {
   var darkModeToggle = document.getElementById('darkMode').checked;
+  var floatingWindowToggle = document.getElementById('floatingWindow').checked;
+  
   chrome.storage.sync.set({ 'darkMode' : darkModeToggle }, function() {
 	if (!(document.getElementById('darkMode').checked)) {
         chrome.tabs.executeScript({
@@ -13,7 +33,18 @@ function save_options() {
     var status = document.getElementById('save');
     status.textContent = 'Options saved.';
     setTimeout(function() {
-      status.textContent = 'Save Settings';
+		status.textContent = 'Save Settings ';
+    }, 1250);
+  });
+  chrome.storage.sync.set({ 'floatingWindow' : floatingWindowToggle }, function() {
+	if ((document.getElementById('floatingWindow').checked)) {
+			load_window();
+			window.close();   
+	}
+    var status = document.getElementById('save');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+		status.textContent = 'Save Settings ';
     }, 1250);
   });
 }
@@ -30,7 +61,16 @@ function load_options() {
 			}); 
 		}
 	});
+	chrome.storage.sync.get('floatingWindow', function(item) {
+		document.getElementById('floatingWindow').checked = item.floatingWindow;
+		if ((document.getElementById('floatingWindow').checked)) {
+			load_window();
+		}
+	});
 }
+chrome.commands.onCommand.addListener(function(command) { 
+	load_window();
+});
 var save = document.getElementById("save");
 save.addEventListener('click', save_options);
 document.addEventListener('DOMContentLoaded', load_options);
